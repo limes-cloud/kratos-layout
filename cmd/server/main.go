b559ce79a9f2de9eb7b5d5cf18f40027b65e8cd6
fangweiye <fangweiye@baidu.com>
2024-03-04 19:02:21 +0800

    feat: change layout

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/go-kratos/kratos/v2/config/file"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/limes-cloud/kratosx"
 	"github.com/limes-cloud/kratosx/config"
-	userpb "github.com/limes-cloud/user-center/api/user/v1"
 	_ "go.uber.org/automaxprocs"
 
 	internalconf "github.com/go-kratos/kratos-layout/internal/conf"
@@ -23,15 +19,6 @@ func main() {
 		kratosx.RegistrarServer(RegisterServer),
 	)
 
-	kCtx := kratosx.MustContext(context.Background())
-	conn, err := kCtx.GrpcConn("UserCenter")
-	if err != nil {
-		panic(err)
-	}
-	userClient := userpb.NewServiceClient(conn)
-	user, err := userClient.GetSimpleUser(kCtx, &userpb.GetSimpleUserRequest{Id: 1})
-
-	fmt.Println(user, err)
 	if err := app.Run(); err != nil {
 		log.Fatal(err.Error())
 	}
